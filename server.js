3803084820b0a688f16a6340c35391c27281ed23
newprice247 <newprice247@gmail.com>
2023-10-01 07:31:28 -0400

    default route working and html retrieved clientside

--- server.js
+++ server.js
@@ -1,5 +1,37 @@
 require('dotenv').config();
 
 const express = require('express')
-const Sequilize = require('sequelize')
+const sequelize = require('./config/connection')
 const session = require('express-session')
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+
+const path = require('path')
+
+const exphbs = require('express-handlebars')
+const routes = require('./controllers')
+
+const app = express()
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: 'Tech Blog Secret',
+    resave: false,
+    saveUninitialized: true
+}
+
+const hbs = exphbs.create({ })
+
+app.use(session(sess))
+
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(routes)
+
+sequelize.sync( ).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on PORT ${PORT}`))
+    })
